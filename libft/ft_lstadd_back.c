de3b206522ebc48ec901cff89637555d08dbf1c0
Eleonora <38229445+eluscha@users.noreply.github.com>
2022-12-18 15:18:29 +0100

    add lstaddback function

--- libft/ft_lstadd_back.c
+++ libft/ft_lstadd_back.c
@@ -2,26 +2,20 @@
 
 #include "libft.h"
 
-t_list	*ft_lstlast(t_list *lst)
+void ft_lstadd_back(t_list **lst, t_list *new)
 {
-	t_list	*current;
-
-	current = lst;
-	if (!current)
-		return (NULL);
-	while (current->next)
-		current = current->next;
-	return (current);
+	ft_lstlast(*lst)->next = new;
 }
-/*
+
 #include <stdio.h>
 int main(void)
 {
 	t_list *node1 = ft_lstnew("content number 1");
 	t_list *node2 = ft_lstnew("content number 2");
 	ft_lstadd_front(&node1, node2);
-	printf("ft_lstlast(node1) returns a node with content %s\n", (char *)ft_lstlast(node1)->content);
 	printf("ft_lstlast(node2) returns a node with content %s\n", (char *)ft_lstlast(node2)->content);
+	t_list *node3 = ft_lstnew("content number 3");
+	ft_lstadd_back(&node2, node3);
+	printf("after addback func. ft_lstlast(node2) returns a node with content %s\n", (char *)ft_lstlast(node2)->content);
 	return (0);
 }
-*/
