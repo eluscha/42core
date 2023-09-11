a92a1bab7de608f1438ee293567eded7308c0c3b
Eleonora Usatikova <eusatiko@c0c4c3.42berlin.de>
2023-09-11 15:03:46 +0200

    Make simple parser

--- pipex/main2.c
+++ pipex/main2.c
@@ -1,53 +1,114 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <string.h>
+#include "ft_split.c"
 
-int main() {
-    int pipefd[2];
-    
-    // Create a pipe
-    if (pipe(pipefd) == -1) {
-        perror("pipe");
-        return 1;
-    }
-    
-    pid_t pid = fork();
-    
-    if (pid == -1) {
-        perror("fork");
-        return 1;
-    }
-    
-    if (pid == 0) {  // Child process
-        // Close the write end of the pipe
-        close(pipefd[1]);
-        
-        // Redirect the read end of the pipe to stdin (file descriptor 0)
-        dup2(pipefd[0], 0);
-        
-        // Close the original read end of the pipe
-        close(pipefd[0]);
-        
-        // Now you can use wc or any other command that reads from stdin
-        execlp("wc", "wc", "-l", NULL);
-        perror("execlp");
-        return 1;
-    } else {  // Parent process
-        // Close the read end of the pipe
-        close(pipefd[0]);
-        
-        dup2(pipefd[1],1);
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
 
-        close(pipefd[1]);
-
-        execlp("ls", "ls", "-l", NULL);
-        perror("execlp");
-    
-        // Wait for the child process to finish
-        wait(NULL);
-    }
-    
-    return 0;
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0' && i < n)
+	{
+		if (s1[i] != s2[i])
+			break ;
+		i++;
+	}
+	if (i < n)
+		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	return (0);
 }
 
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (*((char *)s + i) != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*strcat;
+	size_t	len1;
+	size_t	len2;
+	size_t	i;
+
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	strcat = malloc(len1 + len2 + 1);
+	if (strcat == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len1)
+	{
+		strcat[i] = s1[i];
+		i++;
+	}
+	while (i < len1 + len2 + 1)
+	{
+		strcat[i] = s2[i - len1];
+		i++;
+	}
+	return (strcat);
+}
+
+char *parser(char *cmd, char **envp)
+{
+    char **dirs;
+    char *slash_cmd = ft_strjoin("/", cmd); //free in the end
+    char *full_cmd;
+    while (*envp)
+    {
+        if (ft_strncmp((const char *) *envp, "PATH=", 5) == 0)
+            break ;
+        envp++;
+    }
+    if (!*envp)
+    {
+        free(slash_cmd);
+        return NULL;
+    }
+
+    dirs = ft_split(*envp, ':'); //free in the end
+    int i = 0;
+    while (dirs[i])
+    {
+        full_cmd = ft_strjoin(dirs[i++], slash_cmd);
+        if (access(full_cmd, X_OK) == 0)
+            break ;
+        free(full_cmd);
+    }
+ 
+    if (!dirs[i])
+        full_cmd = NULL;
+    free(slash_cmd);
+    i = 0;
+    while (dirs[i])
+        free(dirs[i++]);
+    free(dirs);
+    return full_cmd;
+}
+
+int main(int argc, char **argv, char **envp)
+{
+    char *full_cmd = parser(argv[2], envp);
+    if (full_cmd)
+    {
+        int pid = fork();
+        if (pid == 0)
+        {
+            char *args[] = {NULL};
+            char *env[] = {NULL};
+            execve(full_cmd, args, env);
+        }
+        wait(NULL);
+        free(full_cmd);
+        return 0;
+    }
+    printf("No such command!\n");
+    return 1;
+}
