246bd033e592bf11a8350182e6227c864be6b185
Eleonora Usatikova <eusatiko@c3a2c7.42berlin.de>
2025-03-31 11:02:40 +0200

    finish ex00

--- cpp/cpp08/ex00/main.cpp
+++ cpp/cpp08/ex00/main.cpp
@@ -8,23 +8,29 @@ int main() {
     std::vector<int> vec(arr, arr + sizeof(arr) / sizeof(int));
     std::list<int> lst(arr, arr + sizeof(arr) / sizeof(int));
 
+    /*
+    //iterator usage to print out items
     for (std::vector<int>::iterator it = vec.begin(); it != vec.end(); ++it) {
         std::cout << *it << " ";
     }
+    */
 
-    int value = 3;
+    int values[] = {3, 103, 10}; //3 tests
 
-    try {
-        std::cout << "Searching for " << value << " in vec" << std::endl;
-        std::cout << std::distance(vec.begin(), easyfind(vec, value)) << std::endl;
-    } catch (const std::exception& e) {
-        std::cerr << "Exception: " << e.what() << std::endl;
-    }
+    for (int i = 0; i < 3; ++i) //3 tests
+    {
+        try {
+            std::cout << "Searching for " << values[i] << " in vec" << std::endl;
+            std::cout << std::distance(vec.begin(), easyfind(vec, values[i])) << std::endl;
+        } catch (const std::exception& e) {
+            std::cerr << "Exception: " << e.what() << std::endl;
+        }
 
-    try {
-        std::cout << "Searching for " << value << " in lst" << std::endl;
-        std::cout << std::distance(lst.begin(), easyfind(lst, value)) << std::endl;
-    } catch (const std::exception& e) {
-        std::cerr << "Exception: " << e.what() << std::endl;
+        try {
+            std::cout << "Searching for " << values[i] << " in lst" << std::endl;
+            std::cout << std::distance(lst.begin(), easyfind(lst, values[i])) << std::endl;
+        } catch (const std::exception& e) {
+            std::cerr << "Exception: " << e.what() << std::endl;
+        }
     }
 }
